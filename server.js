d2063373fb4ac2c6d67ad8baa7fcac8b193e5bbc
SebastianSNZ <sebas_stt@hotmail.com>
2020-10-15 11:06:42 -0600

    Commit desde el lab

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = new express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hola mundo :D</h1>");
+    res.send("<h1>Adiós mundo, sale sopes en vacas D':</h1>");
 });
 
 app.listen(process.env.port || 8080);
